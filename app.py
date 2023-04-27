9699f40401c333cbb423a9113f1982f8ef5f648e
MounikaMurikipudi <129580465+MounikaMurikipudi@users.noreply.github.com>
2023-04-27 11:43:42 +0530

    lasttt

--- app.py
+++ app.py
@@ -325,6 +325,8 @@ def search():
             cursor.close()
             return render_template('resultsearch.html',data=data)
         return render_template('resultsearch.html')
+    else:
+        return redirect(url_for('index'))
 @app.route('/editsemresult',methods=['GET','POST'])
 def editsemresult():
     if session.get('user'):
